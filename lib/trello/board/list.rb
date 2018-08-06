1962223f5c822c2991f40473717b4fc7dc7ad2ac
mtaner <mervetaner88@gmail.com>
2018-08-06 18:20:16 +0100

    Remove unused method

--- lib/trello/board/list.rb
+++ lib/trello/board/list.rb
@@ -11,10 +11,6 @@ module Trello
         @cards = {}
       end
 
-      def update_title(new_title:)
-        @title = new_title
-      end
-
       def add_new_card(card_title:, description:, due_date:)
         card = Card.new(title: card_title, description: description, due_date: due_date)
         @cards[card_title] = card.to_h
@@ -22,10 +18,6 @@ module Trello
 
       def delete_card(card_title:)
         @cards.delete(card_title)
-      end
-
-      def cards_in_priority
-        @cards.sort_by { |name, info| info[:due_date] }
       end
 
       def to_h
@@ -38,6 +30,12 @@ module Trello
 
       def save!
         $redis.set("list/#{title}", Marshal.dump(self))
+      end
+
+      private
+
+      def cards_in_priority
+        @cards.sort_by { |name, info| info[:due_date] }.flatten
       end
     end
   end
